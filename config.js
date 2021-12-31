8de494524e71d3e38e5a94a10d09a74f6bfec306
NicoNicoNil <96900920+NicoNicoNil@users.noreply.github.com>
2021-12-31 14:21:11 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+ titleWeb: "Yêu hay không yêu",
+    introTitle: 'HR cute rơi từ trên thiên đường xuống',
+    introDesc: `Có ngày dài mang tên chờ đợi
+                Có nổi nhớ mang tên chờ mong
+                Có deadline đang dí HR sang năm
+                Có CBĐ vẫn luôn chực chờ mọi người ,
+    btnIntro: '^^ehe^^',
+    title: 'Phải chăng vào HR là sai lầm 🥰',
+    desc: 'Phải chăng một ngày chạy 2 cái deadline là quá khó ',
+    btnYes: 'Sao lại khó chứ, cho thêm 5 cái deadline 1 ngày còn chạy được <33',
+    btnNo: 'Nhiều deadline quá cứu bé TTwTT',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao CBĐ chỉ chọn mỗi em <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Yêu CBĐ thì yêu mà không yêu thì phải yêu CBĐ <33333333',
+    mess: 'Ui CBĐ là nhất lun đó 🥰. Yêu CBĐ nhiều thiệt sự 😘😘',
+    messDesc: 'Tối nay 12h giao thừa, CBĐ lại giao cho HR 3 cái deadline nữa nha :>.',
+    btnAccept: 'Okiiiii lun <3. Giao nhiều vào!! Mình thích lắm',
+    messLink: 'https://github.com/zukahai/Moah-HR' //link mess của các bạn. VD: https://m.HR/mai.yeu
 }
